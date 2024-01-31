837eae14a3c64f0a48108ca32e90a6d92478c53d
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-01-31 10:32:16 +0100

    created task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    date_created = models.DateTimeField(timezone.now())
