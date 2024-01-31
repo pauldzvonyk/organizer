f7d88aa9c35fb6931c613d45c17f867e0622ffef
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-01-31 12:24:05 +0100

    home.html displays a list of tasks

--- task/models.py
+++ task/models.py
@@ -6,10 +6,10 @@ from django.utils import timezone
 class Task(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
+    short_description = models.TextField()
     date_created = models.DateTimeField(timezone.now())
     priority = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.title} | {self.author}"
+        return f"{self.title} (Published by: '{self.author}' on {self.date_created})"
--- task/views.py
+++ task/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Task
 
 
-def home(request):
-    return render(request, 'task/home.html', {})
+# def home(request):
+#     return render(request, 'task/home.html', {})
+
+class HomeView(ListView):
+    model = Task
+    template_name = 'task/home.html'
