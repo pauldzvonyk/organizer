e1cf8bce0a87a38b57bd9ceb6e60f08be0993ba7
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-01-31 10:54:22 +0100

    added __str__ method to Task model + Task to admin pannel

--- task/models.py
+++ task/models.py
@@ -10,3 +10,6 @@ class Task(models.Model):
     date_created = models.DateTimeField(timezone.now())
     priority = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} | {self.author}"
