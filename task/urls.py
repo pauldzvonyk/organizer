aa77d32c9ef99d745e44821cc4b4dab933c63a06
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-04 10:54:49 +0100

    created edit_task.html, wired in views & urls

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, TaskDetailView, AddTaskView
+from .views import HomeView, TaskDetailView, AddTaskView, EditTaskView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('task/<int:pk>', TaskDetailView.as_view(), name='task-detail'),
     path('add_task/', AddTaskView.as_view(), name='add_task'),
+    path('task/edit/<int:pk>', EditTaskView.as_view(), name='edit_task'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from . models import Task
-from .forms import TaskForm
+from .forms import TaskForm, EditForm
 
 
 # def home(request):
@@ -16,9 +16,18 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = 'task/task_detail.html'
 
+
 class AddTaskView(CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'task/add_task.html'
     # fields = '__all__'
     #fields = ('title', 'author', 'short_description', 'priority')
+
+
+class EditTaskView(UpdateView):
+    model = Task
+    form_class = EditForm
+    # no need to define form_class, as it has already been taken care of with UpdateView
+    template_name = 'task/edit_task.html'
+    #fields = ('title', 'title_tag', 'short_description', 'date_created', 'priority', 'completed')
