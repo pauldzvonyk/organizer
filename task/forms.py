d0dfffdaeb51ff9bd3b8078400985f98ce262257
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-04 10:56:37 +0100

    created EditForm in forms.py, minor changes to stiling

--- task/forms.py
+++ task/forms.py
@@ -5,12 +5,33 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('title', 'title_tag', 'author', 'short_description', 'priority')
+        fields = ('title', 'author', 'short_description', 'priority')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the task title...'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'short_description': forms.Textarea(attrs={'class': 'form-control'}),
+            'short_description': forms.Textarea(attrs={'class': 'form-control',
+                                                       'placeholder': 'Enter a short '
+                                                                      'description of the '
+                                                                      'task...'}),
+            'priority': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+"""
+EditForm class is necessary to enable the form view with different widgets
+"""
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('title', 'title_tag', 'short_description', 'priority')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the task title...'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'short_description': forms.Textarea(attrs={'class': 'form-control',
+                                                       'placeholder': 'Enter a short '
+                                                                      'description of the '
+                                                                      'task...'}),
             'priority': forms.TextInput(attrs={'class': 'form-control'}),
         }
