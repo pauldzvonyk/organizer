e172d9a1dac7e3018df63c2a2d07ffcef0cc9f07
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-05 12:56:25 +0100

    added authentication register, login, logout functionality

--- organizer/urls.py
+++ organizer/urls.py
@@ -4,4 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('task.urls')),
+
+    # Order is important, django.contrib.auth.urls first to handle login, logout and registration page urls for us
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
