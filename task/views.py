0897fbb4e91e1450927521b788147d381108100e
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-09 09:52:14 +0100

    task_category slugify

--- task/views.py
+++ task/views.py
@@ -8,18 +8,27 @@ from django.urls import reverse_lazy
 # def home(request):
 #     return render(request, 'task/home.html', {})
 
-class HomeView(ListView):
+
+class CategoryMixin:
+    def get_context_data(self, *args, **kwargs):
+        cat_list = Category.objects.all()
+        context = super().get_context_data(*args, **kwargs)
+        context['cat_list'] = cat_list
+        return context
+
+
+class HomeView(CategoryMixin, ListView):
     model = Task
     template_name = 'task/home.html'
     ordering = ['-date_created']
 
 
-class TaskDetailView(DetailView):
+class TaskDetailView(CategoryMixin, DetailView):
     model = Task
     template_name = 'task/task_detail.html'
 
 
-class AddTaskView(CreateView):
+class AddTaskView(CategoryMixin, CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'task/add_task.html'
@@ -27,7 +36,7 @@ class AddTaskView(CreateView):
     # fields = ('title', 'author', 'short_description', 'priority')
 
 
-class AddCategoryView(CreateView):
+class AddCategoryView(CategoryMixin, CreateView):
     model = Category
     template_name = 'task/add_category.html'
     fields = '__all__'
@@ -39,7 +48,7 @@ def CategoryView(request, cats):
     return render(request, 'task/categories.html', {'cats': cats.title().replace('-', ' '), 'category_tasks': category_tasks})
 
 
-class EditTaskView(UpdateView):
+class EditTaskView(CategoryMixin, UpdateView):
     model = Task
     form_class = EditForm
     # no need to define form_class, as it has already been taken care of with UpdateView
@@ -47,7 +56,7 @@ class EditTaskView(UpdateView):
     # fields = ('title', 'title_tag', 'short_description', 'date_created', 'priority', 'completed')
 
 
-class DeleteTaskView(DeleteView):
+class DeleteTaskView(CategoryMixin, DeleteView):
     model = Task
     template_name = 'task/delete_task.html'
     success_url = reverse_lazy('home')
