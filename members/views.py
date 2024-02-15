b4d4e41efb5c6dcae24d36bb285e0ad680dcf91f
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-15 13:41:08 +0100

    bootstraping edit profile form

--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 
 class UserRegistrationView(generic.CreateView):
@@ -12,7 +12,7 @@ class UserRegistrationView(generic.CreateView):
 
 
 class ProfileUpdateView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
