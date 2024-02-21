d8308eb1182ea65f4d270fd3f8d8abc5fbd2041c
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-02-21 12:07:42 +0100

    Profile dropdown menu

--- members/urls.py
+++ members/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('edit_profile/', ProfileUpdateView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/password-change.html')),
     path('<int:pk>/profile/', ProfilePageView.as_view(), name='user_profile'),
-    path('<int:pk>/edit_profile/', EditProfilePageView.as_view(), name='edit_profile_page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
 ]
 
