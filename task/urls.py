d50011ee9a01d63525d26862003309d09ff02219
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-05-07 10:35:24 +0200

    created task_completed.html

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (AllTasksView, TaskDetailView, AddTaskView, EditTaskView, DeleteTaskView, CategoryView,
-                    AddCommentView, LandingPageView, SearchTaskView, increment_progress)
+                    AddCommentView, LandingPageView, SearchTaskView, increment_progress, TaskCompletedView)
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='home'),
@@ -12,5 +12,7 @@ urlpatterns = [
     path('task/edit/<int:pk>', EditTaskView.as_view(), name='edit_task'),
     path('task/delete/<int:pk>', DeleteTaskView.as_view(), name='delete_task'),
     path('category/<str:cats>/', CategoryView.as_view(), name='category'),
-    path('task/<int:pk>/comment', AddCommentView.as_view(), name='add_comment')
+    path('task/<int:pk>/comment', AddCommentView.as_view(), name='add_comment'),
+    path('task/task_completed/<int:pk>', TaskCompletedView.as_view(), name='task-completed'),
 ]
+
--- task/views.py
+++ task/views.py
@@ -258,3 +258,8 @@ class DeleteTaskView(CategoryMixin, DeleteView):
     model = Task
     template_name = 'task/delete_task.html'
     success_url = reverse_lazy('all-tasks')
+
+
+class TaskCompletedView(CategoryMixin, TemplateView):
+    model = Task
+    template_name = 'task/task_completed.html'
