96a1929747ff1255378a0494878d89e16c77291d
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-05-29 16:11:29 +0200

    stored secret_key in environmental variable

--- organizer/settings.py
+++ organizer/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-bpc*x9bhxoso%ocp=1u$jwp2rx1*slxhwuq@o99b(hl*-4q@ix'
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
